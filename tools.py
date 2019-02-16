32a08611ba02cd41bdb39f77ad29661efdc29236
adrianosorio <adrianosorio101@gmail.com>
2019-02-15 17:19:38 -0800

    Update tools.py
    update

--- tools.py
+++ tools.py
@@ -13,15 +13,18 @@ def flag_format(flag):
 def hex_to_ascii(hex_code):
     return bytearray.fromhex(hex_code).decode()
 
+
 # To convert decimal to binary
 def deci_to_bin(deci):
     return "{0:b}".format(deci)
+
 
 # To convert from Hex to decimal and returns hex code as a float
 def hex_to_deci(hex_code):
     decimal_value = int(hex_code, 16)
     print(decimal_value)
     return decimal_value
+
 
 if "__main__" == __name__:
     flag_format(hex_to_deci("3D"))
